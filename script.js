68b5598bee460d39bf70c2231f99afcc97718d45
catherine91 <u1317921@utah.edu>
2020-11-23 18:03:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,1 @@
+ console.log("Wildfires have been on the rise in recent years. Officials are not sure why the trend has changed, but several factors may be contributing including: temperature changes, increased amount of dry brush, and weather patterns. This website has the most up to date information about wildfire tracking in the US.") 
